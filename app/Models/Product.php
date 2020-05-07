f377aeee2177f10e21e0e5413877c2ec80820c3f
lance <lance@qq.com>
2020-05-07 18:31:44 +0800

    商品属性

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -52,6 +52,7 @@ class Product extends Model
         'title', 'description', 'image', 'on_sale',
         'rating', 'sold_count', 'review_count', 'price',
         'type',
+        'long_title',
     ];
     protected $casts = [
         'on_sale' => 'boolean', // on_sale 是一个布尔类型的字段
@@ -79,5 +80,21 @@ class Product extends Model
     public function crowdfunding()
     {
         return $this->hasOne(CrowdfundingProduct::class);
+    }
+
+    public function properties()
+    {
+        return $this->hasMany(ProductProperty::class);
+    }
+
+    public function getGroupedPropertiesAttribute()
+    {
+        return $this->properties
+            // 按照属性名聚合，返回的集合的 key 是属性名，value 是包含该属性名的所有属性集合
+            ->groupBy('name')
+            ->map(function ($properties) {
+                // 使用 map 方法将属性集合变为属性值集合
+                return $properties->pluck('value')->all();
+            });
     }
 }
